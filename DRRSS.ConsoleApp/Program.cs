0269609b8c8b79ff94047704e11d419d184ad5b0
admin <admin@TA-826-INS>
2020-08-27 14:12:33 +0200

    Add project files.

---
+++ DRRSS.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace DRRSS.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var res = DRRSS.Kerne.Nyhed.HentNyheder(@"https://www.dr.dk/nyheder/service/feeds/allenyheder/");
+            foreach (var item in res)
+            {
+                Console.WriteLine(item.Titel);
+            }
+        }
+    }
+}
---
+++ DRRSS.Kerne/Nyhed.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace DRRSS.Kerne
+{
+    public class Nyhed
+    {
+        public string Titel;
+        public string Beskrivelse;
+        public string Link;
+        public DateTime Dato;
+
+        public static List<Nyhed> HentNyheder(string url)
+        {
+            try
+            {
+                List<Nyhed> lst = new List<Nyhed>();
+                XmlDocument doc = new XmlDocument();
+
+                doc.Load(url);
+                var items = doc.SelectNodes("//item");
+                foreach (XmlNode item in items)
+                {
+                    lst.Add(new Nyhed()
+                    {
+                        Titel = item.SelectSingleNode("title").InnerText,
+                        Beskrivelse = item.SelectSingleNode("description").InnerText,
+                        Link = item.SelectSingleNode("link").InnerText,
+                        Dato = Convert.ToDateTime(item.SelectSingleNode("pubDate").InnerText)
+                    });
+                }
+                return lst.OrderByDescending(i => i.Dato).ToList();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Kan ikke deserialisere" + url, ex);
+            }
+        }
+    }
+}
